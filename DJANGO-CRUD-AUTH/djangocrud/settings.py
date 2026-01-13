8832d82984b8dee7bcfcf3139751f66f622cb451
Javier <javierapo152@gmail.com>
2026-01-12 21:16:54 -0500

    Configuración para Render y Whitenoise

--- DJANGO-CRUD-AUTH/djangocrud/settings.py
+++ DJANGO-CRUD-AUTH/djangocrud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7e%+g7ak4$)^=6wq16wnr(f_xb860su#%#*x#p&+h8t(9-_p5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
